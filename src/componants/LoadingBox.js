2c4cf4f26fa7e9c76f108368c9386a3f48efee8a
Tariq-the-Coder <76946087+Tariq-the-Coder@users.noreply.github.com>
2021-03-27 17:31:55 +0530

    make it better

--- src/componants/LoadingBox.js
+++ src/componants/LoadingBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function LoadingBox() {
     return (
-        <div className="loading row center">
+        <div className=" text-center ">
             <i className="fa fa-spinner fa-spin fa-2x"></i> Loading...
         </div>
     )
